4b39b7b059e1014a7735442d6282064115db8dd4
MetaEMK <Jan-Benecke@t-online.de>
2022-12-05 11:34:31 +0100

    adding routing for structure
    added all subrouter and modules

---
+++ src/app/pages/recipe_variant/recipe-variant.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipeVariantRoutingModule } from './recipe-variant-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecipeVariantRoutingModule
+  ]
+})
+export class RecipeVariantModule { }
